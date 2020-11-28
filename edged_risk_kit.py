ae3d3612f428eda18031a3293a222f25d7d77a24
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-11-27 23:16:46 -0500

    Update edged_risk_kit.py

--- edged_risk_kit.py
+++ edged_risk_kit.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scipy.stats 
 
 def drawdown(return_series: pd.Series):
     # Takes a time series of asset returns.
@@ -14,7 +15,6 @@ def drawdown(return_series: pd.Series):
         "Drawdown": drawdowns
     })
 
-
 def get_ffme_returns():
     #Load the Fama-French Dataset for the returns of the Top and Bottom Deciles 
     #by MarketCap
@@ -27,7 +27,6 @@ def get_ffme_returns():
     return rets
 
 
-
 def get_hfi_returns():
     #Load and Format the EHEC Hedge Fund Index
     hfi = pd.read_csv("/Users/iuliia/Desktop/data/edhec-hedgefundindices.csv", header=0, index_col=0, parse_dates=True)
@@ -35,4 +34,34 @@ def get_hfi_returns():
     hfi.index = hfi.index.to_period('M')
     return hfi
 
+
+def skewness(r):
+    # Alternative to scipy.stats.skew()
+    # Computes the skewness of the supplied Series or DataFrame
+    # Returns a float or a Series
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**3).mean()
+    return exp/sigma_r**3
+
+
+def kurtosis(r):
+    # Alternative to scipy.stats.kurtosis()
+    # Computes the kurtosis of the supplied Series or DataFrame
+    # Returns a float or a Series
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**4).mean()
+    return exp/sigma_r**4
+
+
+def is_normal(r, level=0.01):
+    # Applies the Jarque-Bera test to determine if a Series is normal or not
+    # Test is applied at the 1% level by default
+    # Returns True if the hypothesis of normality is accepted, False otherwise.
     
+    # .jarque_bera returns a tuple
+    statistic, p_value = scipy.stats.jarque_bera(r)
+    return p_value > level
