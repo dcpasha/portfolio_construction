4d7140c8e3bee959a854bfcd83b071e762ee5899
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-11-28 13:05:37 -0500

    Update edged_risk_kit.py

--- edged_risk_kit.py
+++ edged_risk_kit.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import numpy as np
 import scipy.stats 
+from scipy.stats import norm
+
 
 def drawdown(return_series: pd.Series):
-    # Takes a time series of asset returns.
-    # Returns a DataFrame with colums:
-    # for the wealth index, the previos peaks, and
-    # the percentage drawdown
+    """
+    Takes a time series of asset returns.
+    Returns a DataFrame with colums:
+    for the wealth index, the previos peaks, and
+    the percentage drawdown
+    """
     wealth_index = 1000*(1+return_series).cumprod()
     previous_peaks = wealth_index.cummax()
     drawdowns = (wealth_index - previous_peaks)/wealth_index
@@ -16,8 +21,10 @@ def drawdown(return_series: pd.Series):
     })
 
 def get_ffme_returns():
-    #Load the Fama-French Dataset for the returns of the Top and Bottom Deciles 
-    #by MarketCap
+    """
+    Load the Fama-French Dataset for the returns of the Top and Bottom Deciles 
+    by MarketCap
+    """
     me_m = pd.read_csv("/Users/iuliia/Desktop/data/Portfolios_Formed_on_ME_monthly_EW.csv",
                       header=0, index_col=0, na_values=-99.99)
     rets = me_m[['Lo 10', 'Hi 10']]
@@ -28,17 +35,30 @@ def get_ffme_returns():
 
 
 def get_hfi_returns():
-    #Load and Format the EHEC Hedge Fund Index
+    """
+    Load and Format the EHEC Hedge Fund Index
+    """
     hfi = pd.read_csv("/Users/iuliia/Desktop/data/edhec-hedgefundindices.csv", header=0, index_col=0, parse_dates=True)
     hfi = hfi/100
     hfi.index = hfi.index.to_period('M')
     return hfi
 
 
+def semideviation(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series of a DataFrame
+    """
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
+
+
 def skewness(r):
-    # Alternative to scipy.stats.skew()
-    # Computes the skewness of the supplied Series or DataFrame
-    # Returns a float or a Series
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
     demeaned_r = r - r.mean()
     # use the population standard deviation, so set dof=0
     sigma_r = r.std(ddof=0)
@@ -47,9 +67,11 @@ def skewness(r):
 
 
 def kurtosis(r):
-    # Alternative to scipy.stats.kurtosis()
-    # Computes the kurtosis of the supplied Series or DataFrame
-    # Returns a float or a Series
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
     demeaned_r = r - r.mean()
     # use the population standard deviation, so set dof=0
     sigma_r = r.std(ddof=0)
@@ -58,10 +80,74 @@ def kurtosis(r):
 
 
 def is_normal(r, level=0.01):
-    # Applies the Jarque-Bera test to determine if a Series is normal or not
-    # Test is applied at the 1% level by default
-    # Returns True if the hypothesis of normality is accepted, False otherwise.
-    
-    # .jarque_bera returns a tuple
+    """
+    Applies the Jarque-Bera test to determine if a Series is normal or not. 
+    Test is applied at the 1% level by default.
+    Returns True if the hypothesis of normality is accepted, False otherwise.
+    .jarque_bera returns a tuple
+    """ 
     statistic, p_value = scipy.stats.jarque_bera(r)
     return p_value > level
+
+
+def var_historic(r, level=5):
+    """
+    VaR Historic
+    """
+    if isinstance(r, pd.DataFrame):
+        return r.aggregate(var_historic, level=level)
+    elif isinstance(r, pd.Series):
+        return -(np.percentile(r, level))
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
+        
+
+        
+def var_gaussian(r, level=5):
+    """
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    """
+    # Compute the Z score assuming it was Gaussian
+    
+    # Returns the Z-score
+    z = norm.ppf(level/100) # at 5% level.
+    
+    # Z-score tells us how far it is from the mean.
+    return -(r.mean() + z*r.std(ddof=0))
+
+
+
+def var_gaussian(r, level=5, modified=False):
+    """
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    """
+    # Compute the Z score assuming it was Gaussian
+    
+    # Returns the Z-score
+    z = norm.ppf(level/100) # at 5% level.
+    if modified:
+        # modify the Z-score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z +
+                (z**2 -1)*s/6 +
+                (z**3 -3*z)*(k-3)/24 -
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    
+    # Z-score tells us how far it is from the mean.
+    return -(r.mean() + z*r.std(ddof=0))
+
+
+
+def cvar_historic(r, level=5):
+    """
+    Computes the Conditional VaR of Series or DataFrame
+    """
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_historic(r, level=level)
+        return -r[is_beyond].mean()
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_historic, level=level)
+    else:
+        raise TypeError("Expected r to be Series or DataFrame")
