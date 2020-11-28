6f71d7d616f6e8d2ed36b03e71bf63e12ae810de
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-11-27 22:03:19 -0500

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,7 @@
+# We use this extension to reload modules automatically.
+# Magic command starts with a percentage 
+%load_ext autoreload
+%autoreload 2
+
+# To import modules that I created
+import edhec_risk_kit as erk
