d3a149119a871c10f2b8c43b4016ad250edbe23f
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-11-27 22:31:46 -0500

    Update edged_risk_kit.py

--- edged_risk_kit.py
+++ edged_risk_kit.py
@@ -14,6 +14,7 @@ def drawdown(return_series: pd.Series):
         "Drawdown": drawdowns
     })
 
+
 def get_ffme_returns():
     #Load the Fama-French Dataset for the returns of the Top and Bottom Deciles 
     #by MarketCap
@@ -25,5 +26,13 @@ def get_ffme_returns():
     rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
     return rets
 
+
+
+def get_hfi_returns():
+    #Load and Format the EHEC Hedge Fund Index
+    hfi = pd.read_csv("/Users/iuliia/Desktop/data/edhec-hedgefundindices.csv", header=0, index_col=0, parse_dates=True)
+    hfi = hfi/100
+    hfi.index = hfi.index.to_period('M')
+    return hfi
 
     
