471d4b9c8e148b2975fdf034a08498c3c0c729ec
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-12-01 19:10:45 -0500

    Update edged_risk_kit.py

--- edged_risk_kit.py
+++ edged_risk_kit.py
@@ -38,10 +38,23 @@ def get_hfi_returns():
     """
     Load and Format the EHEC Hedge Fund Index
     """
-    hfi = pd.read_csv("/Users/iuliia/Desktop/data/edhec-hedgefundindices.csv", header=0, index_col=0, parse_dates=True)
+    hfi = pd.read_csv("/Users/pavelpotapov/Coursera/Portfolio/notebooks_and_codem01_v02/data/edhec-hedgefundindices.csv", header=0, index_col=0, parse_dates=True)
     hfi = hfi/100
     hfi.index = hfi.index.to_period('M')
     return hfi
+
+def get_ind_returns():
+    """
+    Load and Format the Ken French 30 Industry Portfolio Value Weighter Montly Returns.
+    """
+    # The return of 30 different industry portfolios
+    ind = pd.read_csv('/Users/pavelpotapov/Coursera/Portfolio/notebooks_and_codem01_v02/data/ind30_m_vw_rets.csv',
+                 header=0, index_col=0, parse_dates=True)/100
+    # Converting index to Date
+    ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period('M')
+    # Get rid of spaces in column names.
+    ind.columns = ind.columns.str.strip()
+    return ind
 
 
 def semideviation(r):
@@ -92,14 +105,16 @@ def is_normal(r, level=0.01):
 
 def var_historic(r, level=5):
     """
-    VaR Historic
+    Returns the historic Value at Risk at a specified level
+    i.e. returns the number such that "level" percent of the returns
+    fall below that number, and the (100-level) percent are above
     """
     if isinstance(r, pd.DataFrame):
         return r.aggregate(var_historic, level=level)
     elif isinstance(r, pd.Series):
-        return -(np.percentile(r, level))
+        return -np.percentile(r, level)
     else:
-        raise TypeError("Expected r to be Series or DataFrame")
+        raise TypeError("Expected r to be a Series or DataFrame") 
         
 
         
@@ -116,28 +131,26 @@ def var_gaussian(r, level=5):
     return -(r.mean() + z*r.std(ddof=0))
 
 
-
 def var_gaussian(r, level=5, modified=False):
     """
-    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    Returns the Parametric Gauusian VaR of a Series or DataFrame
+    If "modified" is True, then the modified VaR is returned,
+    using the Cornish-Fisher modification
     """
-    # Compute the Z score assuming it was Gaussian
-    
-    # Returns the Z-score
-    z = norm.ppf(level/100) # at 5% level.
+    # compute the Z score assuming it was Gaussian
+    z = norm.ppf(level/100)
     if modified:
-        # modify the Z-score based on observed skewness and kurtosis
+        # modify the Z score based on observed skewness and kurtosis
         s = skewness(r)
         k = kurtosis(r)
         z = (z +
-                (z**2 -1)*s/6 +
+                (z**2 - 1)*s/6 +
                 (z**3 -3*z)*(k-3)/24 -
                 (2*z**3 - 5*z)*(s**2)/36
             )
-    
+        
     # Z-score tells us how far it is from the mean.
     return -(r.mean() + z*r.std(ddof=0))
-
 
 
 def cvar_historic(r, level=5):
@@ -151,3 +164,70 @@ def cvar_historic(r, level=5):
         return r.aggregate(cvar_historic, level=level)
     else:
         raise TypeError("Expected r to be Series or DataFrame")
+
+        
+
+def annualize_rets(r, periods_per_year):
+    """
+    Annualizes a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    to the reader :-)
+    """
+    compounded_growth = (1+r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth**(periods_per_year/n_periods)-1
+
+def annualize_vol(r, periods_per_year):
+    """
+    Annualizes the vol of a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    to the reader :-)
+    """
+    return r.std()*(periods_per_year**0.5)
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
+
+
+def portfolio_return(weights, returns):
+    """
+    Weights -> Returns
+    """
+    return weights.T @ returns
+
+
+def portfolio_vol(weights, covmat):
+    """
+    Weights -> Vol
+    """
+    return (weights.T @ covmat @ weights)**0.05
+
+
+def plot_ef2(n_points, er, cov, style=".-"):
+    """
+    Plot the 2-asset Efficient Frontier
+    """
+    if er.shape[0] != 2 or er.shape[0] != 2:
+        raise ValueError["plot_ef2 can only plot 2-asset frontiers"]
+    
+    # Gives a sequence of weights
+    weights = [np.array([w, 1-w]) for w in np.linspace(0, 1, n_points)] # linspace => linearly spaces
+
+    # Find a Return and Volatility for each weight.
+    rets = [erk.portfolio_return(w, er[l]) for w in weights]
+    vols = [erk.portfolio_vol(w, cov.loc[l,l]) for w in weights]
+    ef = pd.DataFrame({
+        "Returns":rets,
+        "Volatility":vols
+    })
+    return ef.plot.line(x="Volatility", y="Returns", style=style)
